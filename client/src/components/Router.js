e42c79c091e30795c359da3bf56b956eebdb2f1e
Gregory Rutherford <gregorford@gmail.com>
2019-06-20 20:22:28 -0500

    commiting so I can pull down

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Splash from "./Splash";
 import NotFound from "./NotFound";
 import App from "../App";
 
 const Router = () => (
     <BrowserRouter>
         <Switch>
-            <Route exact path ="/" component={Splash} />
-            <Route path="/store/:storeId" component={App} />
+            <Route exact path="/" component={App} />
             <Route component={NotFound} />
         </Switch>
     </BrowserRouter>
