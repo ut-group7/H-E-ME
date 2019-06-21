0682a9397c1562c4af3173417fa2a4d061f79562
Brock Cowen <brock12cowen@gmail.com>
2019-06-20 20:16:48 -0500

    adding models for sequelize

---
+++ models/aisle.js
@@ -0,0 +1,30 @@
+module.exports = function (sequelize, DataTypes) {
+  var Aisle = sequelize.define("Aisle", {
+    aisle_id: {
+      type: DataTypes.INTERGER,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    store_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      len: [1]
+    }
+  });
+
+  Aisle.associate = function (models) {
+    Aisle.belongsTo(models.Store, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Store;
+};
---
+++ models/store.js
@@ -0,0 +1,27 @@
+module.exports = function(sequelize, DataTypes) {
+    var Store = sequelize.define("Store", {
+      store_id: {
+        type: DataTypes.INTERGER,
+        allowNull: false
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        len: [1]
+      }
+    });
+  
+    Store.associate = function(models) {
+        Store.hasMany(models.List);
+      };
+  
+    return Store;
+  };
+  
