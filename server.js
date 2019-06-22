7fec9d9b509b8b7c16b7662ff22ac1a649945beb
cayce brown <caycebrown98@gmail.com>
2019-06-22 11:53:12 -0500

    removed db.sync force true, updated credentials in config

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use("/api", routes);
 
 // Start the API server
 // ADD SEQUELIZE HERE TO CONNECT TO YOUR DB
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync({}).then(() => {
   app.listen(PORT, () => {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
   });
